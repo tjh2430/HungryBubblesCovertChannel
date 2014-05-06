56fb4b3a4c2be602399944937e7c90c5e0a95484
theard <TimHeard@hotmail.com>
2014-05-05 23:38:35 -0400

    Added project files

---
+++ app/src/main/java/com/example/hungrybubblescovertchannel/app/AppInfo.java
@@ -0,0 +1,100 @@
+package com.example.hungrybubblescovertchannel.app;
+
+import android.app.Application;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.example.intentutils.lib.*;
+
+/**
+ * Manages general application information and game statistics. 
+ * 
+ * @author Timothy Heard, Shaun DeVos, John O'Brien, Mustafa Al Salihi
+ */
+public class AppInfo extends Application
+{
+	public static final int MAX_RADIUS = 125;
+	public static final int MIN_RADIUS = 10; 
+	
+	public static final int PLAYER_STARTING_RADIUS = 30;
+	public static final int PLAYER_TARGET_RADIUS = 100; 
+	public static final int PLAYER_STARTING_DIRECTION = 0;
+
+	public static final int MAX_BUBBLES = 8;
+	
+	public static final int VIRTUAL_PADDING_AMOUNT = MAX_RADIUS * 2;
+	
+	private static final String GAME_DATA = "GAME_DATA";
+	private static final String WINS = "WINS";
+	private static final String TOTAL_GAMES = "TOTAL_GAMES";
+	
+	// Used to persist game statistics when the application is terminated as 
+	// well as to retrieve any statistics from previous games when the 
+	// application starts up
+	private SharedPreferences gameData;
+	
+	// Keeps track of the total number of games won as well as the total number
+	// of games played
+	int totalWins, totalGames;
+	
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		gameData = getSharedPreferences(GAME_DATA, MODE_PRIVATE);
+	    totalWins = gameData.getInt(WINS, 0);
+	    totalGames = gameData.getInt(TOTAL_GAMES, 0);
+	}
+	
+	@Override
+	public void onTerminate()
+	{
+		super.onTerminate();
+		savePersistentData();
+	}
+	
+	public void endGame(boolean win)
+    {
+        String msg;
+        if(win)
+        {
+            msg = "user won";
+            totalWins++;
+        }
+        else
+        {
+            msg = "user lost";
+        }
+
+        totalGames++;
+
+        savePersistentData();
+
+        EncodingSchema schema = new LowerCaseAlphaEncoder();
+
+        // TODO: Use constants instead of hard-coded build version
+        // TODO: Check the input validity
+        Intent encodedIntent = schema.encode(msg, 8).get(0);
+        encodedIntent.setAction(EncodingUtils.RECEIVER_COVERT_MESSAGE_ACTION);
+        startService(encodedIntent);
+    }
+
+	public int getTotalWins()
+	{
+		return totalWins;
+	}
+	
+	public int getTotalGames()
+	{
+		return totalGames;
+	}
+	
+	private void savePersistentData()
+	{
+		SharedPreferences.Editor editor = gameData.edit();
+		editor.putInt(TOTAL_GAMES, totalGames);
+		editor.putInt(WINS, totalWins);
+
+		editor.commit();	
+	}
+}
